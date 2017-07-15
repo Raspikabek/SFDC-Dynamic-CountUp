0265f322844bf679610680729855a4d40260d432
Eduardo Asenjo Rodriguez <easenjo@3nitram.com>
2017-07-15 04:15:33 +0200

    first commit

---
+++ src/aura/CountUp/CountUpController.js
@@ -0,0 +1,43 @@
+({
+	doInit : function(component, event, helper){
+		console.log("Do init!");
+		var myRecordId = component.get("v.recordId");
+		var mySObjectName = component.get("v.sObjectName");
+		var myChildRelationship = component.get("v.childRelation");
+		// JUST FOR TEST
+		if(myRecordId == null) myRecordId = '0010Y00000Wf6XRQAZ';
+		if(mySObjectName == null) mySObjectName = 'Account';
+		if(myChildRelationship == null) myChildRelationship = 'Opportunities';
+		var action = component.get("c.getMaxValue");
+		
+		action.setParams({
+			recordId : myRecordId,
+			field : component.get("v.field"),
+			SObjectName : mySObjectName,
+			childRelation : myChildRelationship,
+			operation : component.get("v.operation")
+		});
+		
+		action.setCallback(this, function(response){
+			var state = response.getState();
+			if(component.isValid && state === "SUCCESS"){
+				component.set("v.maxValue", response.getReturnValue());
+				component.set("v.success", true);
+				console.log("Value returned");
+			}
+			else if(component.isValid && state === "ERROR"){
+				console.log("Something wrong!");
+				var errors = response.getError();
+				if(errors){
+					if(errors[0] && errors[0].message){
+						console.log("Error message: " + errors[0].message);
+					}
+				}
+				else {
+					console.log("Unknown error");
+				}
+			}
+		});
+		$A.enqueueAction(action);
+	}
+})
---
+++ src/aura/CountUp/CountUpRenderer.js
@@ -0,0 +1,6 @@
+({
+	// Your renderer method overrides go here
+	rerender : function(cmp, helper){
+	
+	}
+})
